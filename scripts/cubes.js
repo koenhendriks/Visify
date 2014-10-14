ffd53c3792886772e89ac075387f1d930709319b
Koen Hendriks <info@koenhendriks.com>
2014-10-14 16:29:47 +0200

    Added trackball controls, more colors and more cubes

---
+++ scripts/cubes.js
@@ -0,0 +1,139 @@
+/**
+ * Created by koen on 10/14/14.
+ */
+var container, stats;
+var camera, controls, scene, projector, renderer;
+var objects = [], plane;
+
+
+init();
+animate();
+
+function init() {
+
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+
+    controls = new THREE.TrackballControls( camera );
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+
+    scene = new THREE.Scene();
+
+    scene.add( new THREE.AmbientLight( 0x505050 ) );
+
+    var light = new THREE.SpotLight( 0xffffff, 1.5 );
+    light.position.set( 0, 500, 2000 );
+    light.castShadow = true;
+
+    light.shadowCameraNear = 200;
+    light.shadowCameraFar = camera.far;
+    light.shadowCameraFov = 50;
+
+    light.shadowBias = -0.00022;
+    light.shadowDarkness = 0.5;
+
+    light.shadowMapWidth = 2048;
+    light.shadowMapHeight = 2048;
+
+    scene.add( light );
+
+    var geometry = new THREE.BoxGeometry( 40, 40, 40 );
+
+    for ( var i = 0; i < 200; i ++ ) {
+
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+        object.material.ambient = object.material.color;
+
+        object.position.x = Math.random() * 1000 - 500;
+        object.position.y = Math.random() * 600 - 300;
+        object.position.z = Math.random() * 800 - 400;
+
+        object.rotation.x = Math.random() * 2 * Math.PI;
+        object.rotation.y = Math.random() * 2 * Math.PI;
+        object.rotation.z = Math.random() * 2 * Math.PI;
+
+        object.scale.x = Math.random() * 2 + 1;
+        object.scale.y = Math.random() * 2 + 1;
+        object.scale.z = Math.random() * 2 + 1;
+
+        object.castShadow = true;
+        object.receiveShadow = true;
+
+        scene.add( object );
+
+        objects.push( object );
+
+    }
+
+    plane = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.25, transparent: true, wireframe: true } ) );
+    plane.visible = false;
+    scene.add( plane );
+
+    projector = new THREE.Projector();
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setClearColor( 0xf0f0f0 );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.sortObjects = false;
+
+    renderer.shadowMapEnabled = true;
+    renderer.shadowMapType = THREE.PCFShadowMap;
+
+    container.appendChild( renderer.domElement );
+
+    var info = document.createElement( 'div' );
+    info.style.position = 'absolute';
+    info.style.top = '10px';
+    info.style.width = '100%';
+    info.style.textAlign = 'center';
+    container.appendChild( info );
+
+
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+
+//
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    render();
+    for ( var i = 0; i < objects.length; i ++ ) {
+        objects[i].scale.x = 1+wave;
+        objects[i].scale.y = 1+wave;
+        objects[i].scale.z = 1+wave;
+    }
+
+}
+
+function render() {
+
+    controls.update();
+
+    renderer.render( scene, camera );
+
+}
+W
